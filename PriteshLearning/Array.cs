eddd32fae35da18bb4d2487d753a2426bcc67ade
Pritesh Gondaliya <priteshgondalia@gmail.com>
2023-01-26 17:35:40 -0500

    SearchInsert Place

--- PriteshLearning/Array.cs
+++ PriteshLearning/Array.cs
@@ -8,8 +8,8 @@ namespace PriteshLearning
     {
         static void Main()
         {
-            int[] res = new int[4] { 1, 2, 3, 4 };
-            RunningSum(res);
+            int[] res = new int[4] { 1, 3, 5, 6 };
+            SearchInsert(res, 5);
         }
 
         public static int[] RunningSum(int[] nums)
@@ -24,6 +24,23 @@ namespace PriteshLearning
             }
 
             return result;
+        }
+
+        public static int SearchInsert(int[] nums, int target)
+        {
+            int pivot;
+            int left = 0;
+            int right = nums.Length - 1;
+
+            while (left <= right)
+            {
+                pivot = left + (right - left) / 2;
+                if (nums[pivot] == target) return pivot;
+                if (target < nums[pivot]) right = pivot - 1;
+                else left = pivot + 1;
+            }
+
+            return left;
         }
     }
 }
